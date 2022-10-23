14f606ce8b5e8c63d52749ab61d2e21a52b22850
Marcin Przerwa <marcin.przerwa1404@gmail.com>
2022-10-23 21:50:20 +0200

    commit2

---
+++ Laboratoria/src/Lab1/ZadanieA.java
@@ -0,0 +1,11 @@
+package Lab1;
+
+public class ZadanieA {
+    public static void main(String[ ] args)
+    {
+        int x = 20;
+        System.out.println("Hello World!");
+        for(int i=0; i<x; i++)
+            System.out.println(i);
+    }
+}
